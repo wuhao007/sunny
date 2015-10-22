fe2a4ed55c8a5a38c93271d55fc9b0e0a2a27212
Hao Wu <tohaowu@gmail.com>
2015-10-22 07:14:53 -0700

    new one

--- replacename.py
+++ replacename.py
@@ -2,12 +2,21 @@ import xml.etree.ElementTree as ET
 
 xmlns = "http://schemas.android.com/apk/res/android"
 ET.register_namespace("android", xmlns)
-datafile = "AndroidManifest.xml"
+
+datafile = "AndroidManifest2.xml"
 tree = ET.parse(datafile)
 root = tree.getroot()
 key = "{" + xmlns + "}name"
-
+name = None
 for child in root.iter("application"):
     if key in child.keys():
-        child.set(key, "Yao Chen Hao Wu")
-tree.write(datafile + ".new")
+        name = child.attrib[key]
+print name
+
+datafile = "AndroidManifest1.xml"
+tree1 = ET.parse(datafile)
+root1 = tree1.getroot()
+for child in root1.iter("application"):
+    if key in child.keys():
+        child.set(key, name)
+tree1.write(datafile)
