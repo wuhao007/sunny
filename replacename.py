64a1494cae92d5c0001bd39af6e1ac8fb1917193
Hao Wu <tohaowu@gmail.com>
2015-10-22 07:34:09 -0700

    add path

--- replacename.py
+++ replacename.py
@@ -1,22 +1,29 @@
+import os
 import xml.etree.ElementTree as ET
+
+
+
+datafile2 = os.popen('find path2 -name AndroidManifest.xml').read().strip()
+print datafile2
+
 
 xmlns = "http://schemas.android.com/apk/res/android"
 ET.register_namespace("android", xmlns)
-
-datafile = "AndroidManifest2.xml"
-tree = ET.parse(datafile)
-root = tree.getroot()
 key = "{" + xmlns + "}name"
+
+tree2 = ET.parse(datafile2)
+root2 = tree2.getroot()
 name = None
-for child in root.iter("application"):
+for child in root2.iter("application"):
     if key in child.keys():
         name = child.attrib[key]
 print name
 
-datafile = "AndroidManifest1.xml"
-tree1 = ET.parse(datafile)
+datafile1 = os.popen('find path1 -name AndroidManifest.xml').read().strip()
+print datafile1
+tree1 = ET.parse(datafile1)
 root1 = tree1.getroot()
 for child in root1.iter("application"):
     if key in child.keys():
         child.set(key, name)
-tree1.write(datafile)
+tree1.write(datafile1)
